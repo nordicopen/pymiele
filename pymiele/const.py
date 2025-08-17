2c752c4ec86c34ea401899cd902b367ab00c169b
Mips2648 <mips2648@gmail.com>
2025-08-17 13:18:39 +0200

    add language in API calls

--- pymiele/const.py
+++ pymiele/const.py
@@ -7,3 +7,23 @@ OAUTH2_AUTHORIZE = "https://api.mcs3.miele.com/thirdparty/login"
 OAUTH2_TOKEN = "https://api.mcs3.miele.com/thirdparty/token"
 
 AIO_TIMEOUT = 15
+
+LANG_DA = "da"
+LANG_DE = "de"
+LANG_EN = "en"
+LANG_ES = "es"
+LANG_FR = "fr"
+LANG_IT = "it"
+LANG_NL = "nl"
+LANG_NB = "nb"
+
+AVAILABLE_LANGUAGES = [
+    LANG_DA,
+    LANG_DE,
+    LANG_EN,
+    LANG_ES,
+    LANG_FR,
+    LANG_IT,
+    LANG_NL,
+    LANG_NB,
+]
