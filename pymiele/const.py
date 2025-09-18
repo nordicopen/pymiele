55baf5710c3c754a8cc2e1388a4b3aa5ad2e8ca8
Åke S - Piper <ake@strandberg.eu>
2025-09-18 07:22:24 +0000

    Bump to release 0.5.5

--- pymiele/const.py
+++ pymiele/const.py
@@ -1,6 +1,6 @@
 """Constants for pymiele."""
 
-VERSION = "0.5.5b1"
+VERSION = "0.5.5"
 
 MIELE_API = "https://api.mcs3.miele.com/v1"
 OAUTH2_AUTHORIZE = "https://api.mcs3.miele.com/thirdparty/login"
